b798166e4d0ef0f6c2d12376bf5ae6b091869228
IbrahimGanaMalgwi <ibrahimganamalgwi@outlook.com>
2023-03-08 15:39:09 +0100

    Fetching data with useEffet

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {title: "My new website", body: "Lorem ipsum ...", author: "Kane", id: 1},
-        {title: "Welcome party", body: "Lorem ipsum ...", author: "Jake", id: 2},
-        {title: "Web dev top tips", body: "Lorem ipsum ...", author: "Jane", id : 3}
-    ]);
-   
-    return ( 
-        <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" />           
-        </div>
-    );
+  const [blogs, setBlogs] = useState(null);
+
+
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+    .then(res => {
+        return res.json();
+    })
+    .then((data) => {
+        setBlogs(data);
+    });
+  }, [])
+
+  return (
+    <div className="home">
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+    </div>
+  );
 }
  
 export default Home;
