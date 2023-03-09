46bde42af962e34870ce7b4464c7f28ef294e009
IbrahimGanaMalgwi <ibrahimganamalgwi@outlook.com>
2023-03-09 04:08:14 +0100

    error handling

--- src/Home.js
+++ src/Home.js
@@ -4,21 +4,31 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
 
   useEffect(() => {
     fetch("http://localhost:8000/blogs")
-    .then(res => {
-        return res.json();
-    })
-    .then((data) => {
-        setBlogs(data);
-        setIsPending(false);
-    });
+        .then(res => {
+          if(!res.ok) {
+            throw Error("Cold not fech the data from that resource")
+          }
+            return res.json();
+        })
+        .then((data) => {
+            setBlogs(data);
+            setIsPending(false);
+            setError(null);
+        })
+        .catch(err => {
+            setIsPending(false);
+            setError(err.message);
+        })      
   }, [])
 
   return (
     <div className="home">
+        { error && <div>{ error }</div>} 
         { isPending && <div>Loading...</div>}
         {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
